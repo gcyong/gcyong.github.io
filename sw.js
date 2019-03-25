d7aa7a07747b63616c016611363e9d710b206340
Yongwoo Kim <gcyong@outlook.com>
2019-03-25 10:54:56 +0900

    Modified sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-self.addEventListener('install', function(event) {
-	var CACHE_NAME = 'my-site-cache-v1';
-	var urlsToCache = [
-	  '/',
-	  '/main.css',
-	  '/main.js'
-	];
-	
-	self.addEventListener('install', function(event) {
-	  // Perform install steps
-	  event.waitUntil(
-		caches.open(CACHE_NAME)
-		  .then(function(cache) {
-			console.log('Opened cache');
-			return cache.addAll(urlsToCache);
-		  })
-	  );
-	});
-  });
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
 
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
   self.addEventListener('fetch', function(event) {
 	event.respondWith(
 	  caches.match(event.request)
